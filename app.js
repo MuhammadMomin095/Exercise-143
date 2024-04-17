b1f33cfff4875eb4fd3ac1c5300b11eb71f69ffe
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-04-17 14:49:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var conditionalPromise = new Promise(function (resolve, reject) {
+    var success = Math.random() > 0.5;
+    if (success) {
+        resolve("Success!");
+    }
+    else {
+        reject(new Error("Failure"));
+    }
+});
+conditionalPromise
+    .then(function (result) { return console.log(result); })
+    .catch(function (error) { return console.log(error.message); });
---
+++ app.ts
@@ -0,0 +1,15 @@
+const conditionalPromise = new Promise<string>((resolve, reject) => {
+
+    const success = Math.random() > 0.5;
+
+    if (success) {
+
+        resolve("Success!");
+    } else {
+        reject(new Error ("Failure"));
+    }    
+});
+
+conditionalPromise
+.then((result) => console.log(result))
+.catch((error) => console.log(error.message));
